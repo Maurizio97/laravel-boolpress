af46d78b1707e368fa68d184595ea980714d61b1
Maurizio <proiettomaurizio97@gmail.com>
2022-02-08 14:35:20 +0100

    create the relationship between post and category

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -13,6 +13,18 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        factory(Category::class, 10) -> create();
+        // factory(Category::class, 10) -> create();
+        DB::table('categories')->insert([
+            ['name' => 'categoria 1'],
+            ['name' => 'categoria 2'],
+            ['name' => 'categoria 3'],
+            ['name' => 'categoria 4'],
+            ['name' => 'categoria 5'],
+            ['name' => 'categoria 6'],
+            ['name' => 'categoria 7'],
+            ['name' => 'categoria 8'],
+            ['name' => 'categoria 9'],
+            ['name' => 'categoria 10'],
+        ]);
     }
 }
